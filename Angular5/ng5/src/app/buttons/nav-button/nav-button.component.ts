6ad8ebcbdde0d422212d9c91020ec136d1cb286d
JohnsonLu3 <JohnsonLu94@gmail.com>
2018-06-06 14:27:09 -0400

    button colors and ico now pull from binding

--- Angular5/ng5/src/app/buttons/nav-button/nav-button.component.ts
+++ Angular5/ng5/src/app/buttons/nav-button/nav-button.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-button',
   templateUrl: './nav-button.component.html',
-  styleUrls: ['./nav-button.component.scss']
+  styleUrls: ['./nav-button.component.scss',]
 })
 export class NavButtonComponent implements OnInit {
+
+  @Input() color: string;
+  @Input() name: string;
+  @Input() ico: string;
+  @Input() ref: string;
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  scroll(){
+
   }
 
 }
